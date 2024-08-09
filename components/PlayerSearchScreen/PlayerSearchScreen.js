bcbc3f69c0cab0a9b5c67dc0338fc673dfd5c00d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-09 13:13:33 -0700

    properly passes in params and api key for finding a fortnite user

--- components/PlayerSearchScreen/PlayerSearchScreen.js
+++ components/PlayerSearchScreen/PlayerSearchScreen.js
@@ -2,22 +2,24 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import axios from 'axios';
 import Styles from './styles';
+import Constants from 'expo-constants';
 
 const PlayerSearchScreen = () => {
+    const { apiUrl, apiKey } = Constants.expoConfig.extra;
     const [username, setUsername] = useState('');
     const [result, setResult] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-
     const handleSearch = async () => {
         setLoading(true);
         setError(null);
         try {
-            const response =  await axios.get(`https://fortnite-api.com/v2/stats/br/v2?name=${username}`, {
-               
+            const response = await axios.get(`${apiUrl}/v2/stats/br/v2?name=${username}`, {
+               headers: {
+                "Authorization": apiKey,
+               }
             });
-            alert(response);
             setResult(response.data);
         } catch (err) {
             setError('Error fetching player');
@@ -47,7 +49,6 @@ const PlayerSearchScreen = () => {
             {result && (
                 <View style={Styles.resultContainer}>
                     <Text style={Styles.resultText}>Player Found:</Text>
-                    {console.log(result.username)}
                     {/* <Text style={Styles.resultText}>{result.username}</Text> */}
                 </View>
             )}
