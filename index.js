4c4926a85b240860675020db042ddf62e790a8f3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-17 18:16:16 -0700

    creates centralized component import file

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as PlayerDetailsScreen } from './components/PlayerDetailsScreen/PlayerDetailsScreen';
+export { default as PlayerSearchScreen } from './components/PlayerSearchScreen/PlayerSearchScreen';
+export { default as HomeScreen } from './components/HomeScreen/HomeScreen';
